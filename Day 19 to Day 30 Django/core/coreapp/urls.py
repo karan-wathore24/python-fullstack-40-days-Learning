f2a2c8676161b3e060aa99832f4bd3a671f611c8
karan-wathore24 <karanwathore2002@gmail.com>
2026-01-02 00:46:01 +0530

    Day 20: Django URLs, views, URL parameters and template rendering

--- Day 19 to Day 30 Django/core/coreapp/urls.py
+++ Day 19 to Day 30 Django/core/coreapp/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    path('about',views.about, name='about'),
+    path('contact',views.contact, name="contact"),
 ]
