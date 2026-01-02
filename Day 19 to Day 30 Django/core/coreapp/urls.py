582196808492d005efb7c2b3ec45e837e0814b4c
karan-wathore24 <karanwathore2002@gmail.com>
2026-01-02 23:31:02 +0530

    Day 21: display student list using views, urls and templates

--- Day 19 to Day 30 Django/core/coreapp/urls.py
+++ Day 19 to Day 30 Django/core/coreapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('about',views.about, name='about'),
     path('contact',views.contact, name="contact"),
+    path('add/', views.add_student),
+    path('students/', views.student_list, name='student_list'),
 ]
--- Day 19 to Day 30 Django/core/coreapp/views.py
+++ Day 19 to Day 30 Django/core/coreapp/views.py
@@ -10,3 +10,18 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+from django.http import HttpResponse
+from .models import Student
+
+def add_student(request):
+    Student.objects.create(
+        name="Rahul",
+        email="rahul@gmail.com",
+        age=22
+    )
+    return HttpResponse("Student Added Successfully")
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students.html', {'students': students})
